5034ec1895ceb3b11b0354199e03d04f91e68ed8
Ratchagarn Naewbuntad <ratchagarn@omise.co>
2017-08-02 02:05:06 +0700

    Add global style and update layout style.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,26 @@
-import styled from 'styled-components';
+import styled, { injectGlobal } from 'styled-components';
 
-const Layout = styled.div`
-  font-family: Arial, Verdana, Tahoma;
-  font-size: 16px;
-  box-sizing: border-box;
+injectGlobal`
+  html {
+    box-sizing: border-box;
+  }
 
   *, *:before, *:after {
     box-sizing: inherit;
   }
+
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: Arial, Verdana, Tahoma;
+    font-size: 16px;
+  }
+`;
+
+const Layout = styled.div`
+  width: 95%;
+  margin-left: auto;
+  margin-right: auto;
 `;
 
 export default Layout;
