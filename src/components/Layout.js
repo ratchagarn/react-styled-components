1da4f0e2e08a9d81f5d28ef1771317b1f98b5093
Ratchagarn Naewbuntad <ratchagarn@omise.co>
2017-08-01 17:39:26 +0700

    Update Layout style.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,6 +3,11 @@ import styled from 'styled-components';
 const Layout = styled.div`
   font-family: Arial, Verdana, Tahoma;
   font-size: 16px;
+  box-sizing: border-box;
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
 `;
 
 export default Layout;
